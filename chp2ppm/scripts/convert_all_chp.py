2af0e259772c11982a9e90429193bb13b4b77609
Martin Burchell <martinburchell@spamcop.net>
2012-12-16 18:36:18 +0000

    Replaced chp2raw with chp2ppm

---
+++ chp2ppm/scripts/convert_all_chp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+from glob import iglob
+
+disc_a = os.path.join('/mnt', 'iagb_a')
+disc_b = os.path.join('/mnt', 'iagb_b')
+
+chp_dirs = (os.path.join(disc_a, 'Pictures', 'SCALES', '50K'),
+            os.path.join(disc_a, 'Pictures', 'Towns', 'LRG'),
+            os.path.join(disc_b, 'Pictures', 'SCALES', '25k'))
+
+ppm_dir = os.path.join('..', '..', 'ppm')
+
+for chp_dir in chp_dirs:
+    print chp_dir
+    for chp_path in iglob(os.path.join(chp_dir, '*.chp')):
+        print chp_path
+        (head, tail) = os.path.split(chp_path)
+        (map_name, ext) = os.path.splitext(tail)
+        ppm_name = map_name + '.ppm'
+        ppm_file = os.path.join(ppm_dir, ppm_name)
+        command = "./chp2ppm %s %s" % (chp_path, ppm_file)
+        os.system(command)
---
+++ chp2ppm/src/chp2ppm.c
@@ -0,0 +1,164 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HEADER_SIZE 0x740
+#define PALETTE_OFFSET 0x20
+#define TILE_OFFSET (PALETTE_OFFSET + 0x100 * 4)
+
+static unsigned char g_header[HEADER_SIZE];
+
+static unsigned int chp2ppm_read_int32( unsigned char *buffer )
+{
+    return (unsigned int)( (unsigned int)(buffer[0]) + 
+                           ((unsigned int)(buffer[1]) << 8) + 
+                           ((unsigned int)(buffer[2]) << 16) + 
+                           ((unsigned int)(buffer[3]) << 24));
+}
+
+static void chp2ppm_write_line(FILE *out_fp, const char *line)
+{
+    fprintf(out_fp, "%s%c", line, 0x0a);
+}
+
+static int chp2ppm_process( FILE *in_fp,
+                            FILE *out_fp )
+{
+    int rc = -1;
+    unsigned int num_read;
+    unsigned int num_tiles_x;
+    unsigned int num_tiles_y;
+    unsigned int padded_tile_width;
+    unsigned int tile_width;
+    unsigned int tile_height;
+    unsigned int tile_size;
+    unsigned int tile_y;
+
+    char tile_metadata[100];
+
+    num_read = fread( g_header, HEADER_SIZE, 1, in_fp );
+    if ( num_read < 1 )
+    {
+        fprintf( stderr, "Not a valid CHP file - header too short\n" );
+
+        goto out;
+    }
+
+    if ( strncmp( (const char *)g_header, "PCv", 3 ) != 0 )
+    {
+        fprintf( stderr, "Not a valid CHP file - no marker found\n" );
+
+        goto out;
+    }
+
+    num_tiles_x = chp2ppm_read_int32( g_header + 0x08 );
+    num_tiles_y = chp2ppm_read_int32( g_header + 0x0c );
+
+    tile_width = chp2ppm_read_int32( g_header + 0x10 );
+    tile_height = chp2ppm_read_int32( g_header + 0x14 );
+
+    tile_size = chp2ppm_read_int32( g_header + 0x18 );
+
+    /* round up to nearest 16 */
+    padded_tile_width = (tile_width + 16 - 1) & ~(16 - 1); 
+
+    chp2ppm_write_line(out_fp, "P6");
+    chp2ppm_write_line(out_fp, "# CREATOR: CHP2PPM");
+
+    sprintf(tile_metadata, "%d %d",
+            num_tiles_x * tile_width, num_tiles_y * tile_height);
+    chp2ppm_write_line(out_fp, tile_metadata); 
+    chp2ppm_write_line(out_fp, "255");
+                       
+
+    printf("Tile width: %u\n", tile_width);
+    printf("Tile height: %u\n", tile_height);
+    printf("Tile size: %u\n", tile_size);
+    printf("Tiles %u x %u\n", num_tiles_x, num_tiles_y);
+
+    for ( tile_y = 0; tile_y < num_tiles_y; tile_y++ )
+    {
+        int row;
+
+        for ( row = tile_height-1; row >= 0; row-- )
+        {
+            int tile_index;
+            unsigned int tile_x;
+
+            tile_index = tile_y * num_tiles_x;
+
+            for ( tile_x = 0; tile_x < num_tiles_x; tile_x++ )
+            {
+                unsigned int column;
+                unsigned int tile_start;
+
+                tile_start = chp2ppm_read_int32( g_header + TILE_OFFSET + tile_index * 8 );
+                fseek( in_fp, tile_start + row * padded_tile_width, SEEK_SET );
+
+                for ( column = 0; column < tile_width; column++ )
+                {
+                    int colour = fgetc( in_fp );
+                    const unsigned char *rgb = g_header + PALETTE_OFFSET + colour * 4;
+                    int blue = *( rgb );
+                    int green = *( rgb + 1 );
+                    int red = *( rgb + 2 );
+
+                    fputc( red, out_fp );
+                    fputc( green, out_fp );
+                    fputc( blue, out_fp );
+                }
+
+                tile_index ++;
+            }
+        }
+    }
+
+    rc = 0;
+
+out:
+    return rc;
+}
+
+int main( int argc, char *argv[] )
+{
+    int rc;
+
+    FILE *in_fp;
+    FILE *out_fp;
+
+    if ( argc < 3 )
+    {
+        fprintf( stderr, "Usage: %s <file_in> <file_out>\n", argv[0] );
+        exit( -1 );
+    }
+
+    in_fp = fopen( argv[1], "r" );
+
+    if ( !in_fp )
+    {
+        fprintf( stderr,
+                 "Unable to open file '%s' for read\n",
+                 argv[1] );
+        exit(-1);
+    }
+
+    out_fp = fopen( argv[2], "w" );
+
+    if ( !out_fp )
+    {
+        fclose( in_fp );
+
+        fprintf( stderr,
+                 "Unable to open file '%s' for write\n",
+                 argv[2] );
+        exit(-1);
+    }
+
+    rc = chp2ppm_process( in_fp, out_fp );
+
+    fclose( in_fp );
+    fclose( out_fp );
+
+    return rc;
+}
